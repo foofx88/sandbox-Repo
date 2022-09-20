ff8ebc87f97e15ec03930e847a1ebe66efc54914
fangxuan.foo@gmail.com <FXFoo@AyeSus.(none)>
2022-09-20 20:25:34 +0800

    Wow added!

--- intro.py
+++ intro.py
@@ -9,6 +9,6 @@ def hello(first_name:str=None, last_name:str=None):
     elif not last_name:
         print(f'Hello {first_name}!')
     else:
-        print(f'Hello {first_name} {last_name}!')
+        print(f'Wow! {first_name} {last_name}!')
 
 hello(first_name, last_name)
