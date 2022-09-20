0dc44b8fe6af2f2bb6304cdae483129ad94fc589
fangxuan.foo@gmail.com <FXFoo@AyeSus.(none)>
2022-09-20 19:18:24 +0800

    3rd Commit!

--- intro.py
+++ intro.py
@@ -3,7 +3,9 @@ first_name = input('Enter your First Name:')
 last_name = input('Enter your Last Name:')
 
 def hello(first_name:str=None, last_name:str=None):
-    if not first_name or last_name:
-        print("Wow Nameless!")
+    if not last_name:
+        print(f'Hello {first_name}!')
     else:
         print(f'Hello {first_name} {last_name}!')
+
+hello(first_name, last_name)
