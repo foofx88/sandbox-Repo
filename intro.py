2d53505b20253eec67a5913f0c4ec15ea2bacc80
FX Foo <fangxuan.foo@gmail.com>
2022-09-20 19:12:53 +0800

    Pull Test

--- intro.py
+++ intro.py
@@ -2,5 +2,8 @@ first_name = input('Enter your First Name:')
 
 last_name = input('Enter your Last Name:')
 
-def hello(first_name:str, last_name:str=None):
-    print(f'Hello {first_name} {last_name}!')
+def hello(first_name:str=None, last_name:str=None):
+    if not first_name or last_name:
+        print("Wow Nameless!")
+    else:
+        print(f'Hello {first_name} {last_name}!')
